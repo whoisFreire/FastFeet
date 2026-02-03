1cf2647ca2b3a479d767cbbe310d774ff3d3c8c3
Leonardo Freire Russi <leonardo.russi@blip.ai>
2026-02-02 23:43:33 -0300

    feat: setup envModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { EnvModule } from './infra/env/env.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    }),
+    EnvModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
+  await app.listen(port)
 }
 bootstrap()
